d0770a8ae5d631a1031cdbf85d3e657d4c094aaa
leonidbogin <65702576+leonidbogin@users.noreply.github.com>
2020-11-30 14:26:38 +0300

    Create class Product

---
+++ static-field-in-generic-type/static-field-in-generic-type/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace static_field_in_generic_type
+{
+    public abstract class Product
+    {
+        public string Name { get; set; }
+
+        public Product()
+        {
+
+        }
+
+        public Product(string name)
+        {
+            Name = name;
+        }
+    }
+}
