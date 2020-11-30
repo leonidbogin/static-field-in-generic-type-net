a28df86ea667fef9c3e62bb888a3fea9af538f70
leonidbogin <65702576+leonidbogin@users.noreply.github.com>
2020-11-30 14:26:45 +0300

    Create class Milk

---
+++ static-field-in-generic-type/static-field-in-generic-type/Milk.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace static_field_in_generic_type
+{
+    internal class Milk : Product
+    {
+        public Milk()
+        {
+
+        }
+
+        public Milk(string name) : base(name)
+        {
+
+        }
+    }
+}
