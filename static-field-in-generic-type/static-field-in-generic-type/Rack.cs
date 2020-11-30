889e633645775c8447a0d7b8e30efb9ed8e95922
leonidbogin <65702576+leonidbogin@users.noreply.github.com>
2020-11-30 14:27:03 +0300

    Create class Rack

---
+++ static-field-in-generic-type/static-field-in-generic-type/Rack.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace static_field_in_generic_type
+{
+    public class Rack<T> where T : Product
+    {
+        private List<Product> Products { get; set; }
+        public static int Count{ get; set; }
+
+        public Rack()
+        {
+            Products = new List<Product>();
+        }
+
+        public void Add(T product)
+        {
+            Products.Add(product);
+            Count++;
+        }
+    }
+}
