a44905330fb31cc31700bfd449cc6399435cccde
leonidbogin <65702576+leonidbogin@users.noreply.github.com>
2020-11-30 14:27:37 +0300

    Add sample code to Program

--- static-field-in-generic-type/static-field-in-generic-type/Program.cs
+++ static-field-in-generic-type/static-field-in-generic-type/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace static_field_in_generic_type
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var rackBread = new Rack<Bread>();
+            var rackBread2 = new Rack<Bread>();
+            var rackMilk = new Rack<Milk>();
+
+            for (var i = 0; i < 50; i++)
+            {
+                rackBread.Add(new Bread("Bread " + i));
+            }
+            for (var i = 0; i < 45; i++)
+            {
+                rackBread2.Add(new Bread("Bread " + i));
+            }
+            for (var i = 0; i < 60; i++)
+            {
+                rackMilk.Add(new Milk("Milk " + i));
+            }
+
+            Console.WriteLine("Bread: " + Rack<Bread>.Count);
+            Console.WriteLine("Milk: " + Rack<Milk>.Count);
+
+            Console.ReadLine();
         }
     }
 }
