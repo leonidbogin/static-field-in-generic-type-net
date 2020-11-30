3501474e3aea3af26ad074d9c7af0c0e8741a918
leonidbogin <65702576+leonidbogin@users.noreply.github.com>
2020-11-30 03:36:34 +0300

    Create console projects

---
+++ static-field-in-generic-type/static-field-in-generic-type/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace static_field_in_generic_type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
