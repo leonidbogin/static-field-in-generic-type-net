8f409e28153bc7c723095db92616871315ad2f0e
leonidbogin <65702576+leonidbogin@users.noreply.github.com>
2020-11-30 14:26:52 +0300

    Create class Bread

---
+++ static-field-in-generic-type/static-field-in-generic-type/Bread.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace static_field_in_generic_type
+{
+    internal class Bread : Product
+    {
+        public Bread()
+        {
+
+        }
+
+        public Bread(string name) : base(name)
+        {
+   
+        }
+    }
+}
